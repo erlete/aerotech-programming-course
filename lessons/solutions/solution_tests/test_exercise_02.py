17b92c060a9782b15f76986212eb0301b790514e
Paulo Sánchez <dev.szblzpaulo@gmail.com>
2023-03-07 20:16:10 +0100

    Fix exercise 2 correction

--- lessons/solutions/solution_tests/test_exercise_02.py
+++ lessons/solutions/solution_tests/test_exercise_02.py
@@ -8,6 +8,7 @@ def test():
     unknown = getattr(module, "unknown", None)
     assert unknown == "What am I?", "The variable `unknown` has been modified."
 
-    known = getattr(module, "known", None)
-    assert known is not None, "You have not defined the variable `known`."
-    assert isinstance(known, str), f"The variable `known` has got type {type(known).__name__}, expected `str`."
+    known_type = getattr(module, "known_type", None)
+    assert known_type is not None, "You have not defined the variable `known_type`."
+    assert isinstance(known_type, type), f"The variable `known_type` has got type {type(known_type).__name__}, expected `type`."
+    assert known_type == str, "The variable `known_type` has the wrong value."
