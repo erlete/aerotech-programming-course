5dd4e04155a72818ed54fcafc0ea2eed44063fa1
Matteo Guiotto <116626788+MatteoGuiot@users.noreply.github.com>
2023-10-25 16:50:29 +0200

    Ejercicios_operators

--- lessons/operators/exercises/exercise_13.py
+++ lessons/operators/exercises/exercise_13.py
@@ -1,2 +1,13 @@
 vector_1 = (4, 5.4213)  # Do not modify this line.
 vector_2 = (123, -412.21)  # Do not modify this line.
+
+suma1 = (vector_2[0] - vector_1[0]) ** 2
+suma2 = (vector_2[1] - vector_1[1]) ** 2
+
+print(suma1)
+print(suma2)
+print(suma1 + suma2)
+
+distance = (suma1 + suma2) ** (1/2)
+
+print(distance)
--- lessons/operators/exercises/exercise_15.py
+++ lessons/operators/exercises/exercise_15.py
@@ -1,2 +1,4 @@
 value_1 = -1  # Do not modify this line.
 value_2 = 5  # Do not modify this line.
+
+comp = value_1 > 0 & value_2 > value_1 
