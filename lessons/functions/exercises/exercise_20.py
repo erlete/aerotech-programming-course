4e680d9b481f110300d0e712a77254a58d64c530
Matteo Guiotto <116626788+MatteoGuiot@users.noreply.github.com>
2023-10-25 17:57:43 +0200

    Ejercicios_functions

---
+++ lessons/functions/exercises/exercise_20.py
@@ -0,0 +1,2 @@
+def can_buy(cost, balance=0):
+    return cost <= balance
---
+++ lessons/functions/exercises/exercise_21.py
@@ -0,0 +1,8 @@
+def sum_up(*args):
+    sum = 0
+    
+    for i in args:
+        sum += i
+        
+    sum = sum * 2
+    return sum
