ed678e5e6006f01d326587730da53de2fe44ede0
Matteo Guiotto <116626788+MatteoGuiot@users.noreply.github.com>
2023-10-26 17:43:01 +0200

    Update exercise_23.py

--- lessons/classes/exercises/exercise_23.py
+++ lessons/classes/exercises/exercise_23.py
@@ -8,42 +8,14 @@ class Car:
         self._license_plate = license_plate
         self.__vin_number = vin_number
 
-    @property
-    def color(self):
-        """Get Car color.
-
-        Return:
-            str: color.
-        """
-        return self.color
-
-    @property
-    def license_plate(self):
-        """Get Car license_plate.
-
-        Return:
-            str: license_plate.
-        """
-        return self._license_plate
-
-    @property
-    def vin_number(self):
-        """Get Car vin_number.
-
-        Return:
-            int: vin_number.
-        """
-        return self.__vin_number
-
-    @color.setter
-    def color(self, value):
-        if not isinstance(value, str):
-            return TypeError("the value must be a 'str' type")
-
-        self.color = value
-
 
 mycar = Car("red", "1234ABCD", 12340987)
-ext_color = mycar.color()
-ext_license_plate = mycar.license_plate()
-ext_vin_number = mycar.vin_number()
+
+
+ext_color = mycar.color
+ext_license_plate = mycar.__getattribute__('_license_plate')
+ext_vin_number = mycar.__getattribute__('_Car__vin_number')
+
+print(ext_color)
+print(ext_license_plate)
+print(ext_vin_number)
