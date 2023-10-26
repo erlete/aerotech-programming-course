c8ede4ceb86dc842480a2222d2e3f1b2d4a1fd57
gdfitti <giovannicodf@gmail.com>
2023-10-26 17:04:49 +0200

    ejs

--- lessons/classes/exercises/exercise_22.py
+++ lessons/classes/exercises/exercise_22.py
@@ -1,23 +1,24 @@
+"""Module."""
+
+
 class Coordinate:
     """Class Coordinate.
-    
+
     This class represent a real coordinate realy useful for
     calculate distance between coordinates and represent figures like
     squares, triangles, rectangles and others.
-    
-    Attributes_:
+
+    Attributes:
         x (float): coordinate x.
         y (float): coordinate y.
     """
-    
+
     def __init__(self, x, y):
         """Initialize a Coordinate instance.
 
-        Args_:
+        Args:
             x (float): coordinate x.
             y (float): coordinate y.
         """
         self.x = x
         self.y = y
-    
-
