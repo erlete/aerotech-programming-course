170e782a181708c37167c58c7d1cf8a1c506ab5a
Matteo Guiotto <116626788+MatteoGuiot@users.noreply.github.com>
2023-10-26 16:23:23 +0200

    Update exercise_25.py

--- lessons/exceptions/exercises/exercise_25.py
+++ lessons/exceptions/exercises/exercise_25.py
@@ -1,3 +1,19 @@
 def weird_number(arg):
     """Cheks if arg is int, float, or str."""
 
+    if not isinstance(arg, (int, float, str)):
+        raise TypeError("this function only accepts integers, floats and str.")
+
+    try:
+        arg = float(arg)
+
+    except:
+        arg = 0
+
+    else:
+        arg *= arg * arg
+
+    finally:
+        arg + 1
+
+    return arg
