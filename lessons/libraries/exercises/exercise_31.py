49b40a6014df5a52d2a26799c701551e2f281419
gdfitti <giovannicodf@gmail.com>
2023-10-27 20:14:29 +0200

    ejs

--- lessons/libraries/exercises/exercise_31.py
+++ lessons/libraries/exercises/exercise_31.py
@@ -1,3 +1,16 @@
 import numpy as np
 import scipy.integrate as integrate
+from time import perf_counter as pc
 
+curr = pc()
+riemann_integral = sum(np.sin(np.arange(0, np.pi/2, .05))*0.05)
+t1 = pc() - curr
+curr = pc()
+f = lambda x: np.sin(x)
+scipy_integral, error = integrate.quad(f, 0, np.pi/2)
+t2 = pc() - curr
+
+print(riemann_integral)
+print("\nRiemann: ", t1)
+print(scipy_integral)
+print("\nScipy: ", t2)
---
+++ lessons/libraries/exercises/exercise_32.py
@@ -0,0 +1,41 @@
+import scipy
+import numpy as np
+import matplotlib.pyplot as plt
+from time import perf_counter as pc
+
+
+def splitter():
+    img = scipy.datasets.face()
+
+    plt.imshow(img)
+    plt.show()
+
+    red = np.array(img)
+    green = np.array(img)
+    blue = np.array(img)
+    split = []
+
+    count = pc()
+
+    red[:, :, 1:3] = 0
+    green[:, :, 0:3:2] = 0
+    blue[:, :, -3:-1] = 0
+
+
+
+    time = pc() - count
+    split.append(red)
+    split.append(green)
+    split.append(blue)
+
+    plt.imshow(red)
+    plt.show()
+    plt.imshow(green)
+    plt.show()
+    plt.imshow(blue)
+    plt.show()
+    print(time)
+    return split
+
+
+ej = splitter()
