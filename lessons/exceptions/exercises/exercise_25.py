8fce13094b9ff91986fa866d41926d34f1884c44
gdfitti <giovannicodf@gmail.com>
2023-10-29 19:08:40 +0100

    d

--- lessons/exceptions/exercises/exercise_25.py
+++ lessons/exceptions/exercises/exercise_25.py
@@ -1,12 +1,19 @@
-def weird_number(arg):
+def weird_number(arg: (float, int)) -> float:
+    """Get the arg as float.
+
+    Args:
+        arg (float, int): the weird number.
+
+    Returns:
+        float:the weird number as float.
+    """
     if not isinstance(arg, (int, float, str)):
         raise TypeError("this function only accepts integers, floats and strings.")
-    
+
     try:
-        arg = float(arg)
+        arg: float = float(arg)
     except:
-        arg = 0
-        arg = float(arg)
+        arg = 0.0
     else:
         arg = arg**3
     finally:
