d1ff050ffb4d1df9e1048d39fe1a6966aaf978d1
Paulo Sánchez <dev.szblzpaulo@gmail.com>
2023-11-19 20:45:57 +0100

    Add Python content cards and fix import path

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,8 @@
-import { generateURL } from "./issue_report.js";
+import { generateURL } from "./modules/issue_report.js";
 
 Handlebars.partials = Handlebars.templates;
 
-const content_cards = [
+const envContentCards = [
     {
         img_source: "./public/icons8/git.png",
         title: "Git setup",
@@ -33,13 +33,86 @@ const content_cards = [
     }
 ];
 
-const environmentSetup = {
+const envContentCardsDiv = {
     title: "Progression steps",
     description: "The following steps will guide you through the process of setting up your environment. Try to follow them in order to avoid any issues.",
-    content_cards: content_cards
+    content_cards: envContentCards
 };
 
 let template = Handlebars.templates.content_card_div;
-document.getElementById("env_content_cards").outerHTML = template(environmentSetup);
+document.getElementById("env_content_cards").outerHTML = template(envContentCardsDiv);
 
+const pyContentCards = [
+    {
+        img_source: "./public/icons8/data-types.png",
+        title: "Data types",
+        description: "Learn about the different data types in Python",
+        issue_link: generateURL("python", "data"),
+        link: ""
+    },
+    {
+        img_source: "./public/icons8/operators.png",
+        title: "Operators",
+        description: "Learn about the different operators in Python",
+        issue_link: generateURL("python", "operators"),
+        link: ""
+    },
+    {
+        img_source: "./public/icons8/conditional-structures.png",
+        title: "Conditional structures",
+        description: "Learn about the different conditional structures in Python",
+        issue_link: generateURL("python", "conditional"),
+        link: ""
+    },
+    {
+        img_source: "./public/icons8/iteration-structures.png",
+        title: "Iteration structures",
+        description: "Learn about the different iteration structures in Python",
+        issue_link: generateURL("python", "iteration"),
+        link: ""
+    },
+    {
+        img_source: "./public/icons8/functions.png",
+        title: "Functions",
+        description: "Learn about the different functions in Python",
+        issue_link: generateURL("python", "functions"),
+        link: ""
+    },
+    {
+        img_source: "./public/icons8/classes.png",
+        title: "Classes",
+        description: "Learn about the different classes in Python",
+        issue_link: generateURL("python", "classes"),
+        link: ""
+    },
+    {
+        img_source: "./public/icons8/exceptions.png",
+        title: "Exceptions",
+        description: "Learn about the different exceptions in Python",
+        issue_link: generateURL("python", "exceptions"),
+        link: ""
+    },
+    {
+        img_source: "./public/icons8/libraries.png",
+        title: "Libraries",
+        description: "Learn about the different libraries in Python",
+        issue_link: generateURL("python", "libraries"),
+        link: ""
+    },
+    {
+        img_source: "./public/icons8/code-style.png",
+        title: "Code style",
+        description: "Learn about the different code styles in Python",
+        issue_link: generateURL("python", "codestyle"),
+        link: ""
+    }
+];
 
+const pyContentCardsDiv = {
+    title: "Progression steps",
+    description: "The following steps will guide you through the process of learning Python. Try to follow them in order to avoid any issues.",
+    content_cards: pyContentCards
+};
+
+template = Handlebars.templates.content_card_div;
+document.getElementById("py_content_cards").outerHTML = template(pyContentCardsDiv);
