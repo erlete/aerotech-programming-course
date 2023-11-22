5535dd7e53b25bce91ecdc1e6671c042b996944b
Paulo Sánchez <dev.szblzpaulo@gmail.com>
2023-11-22 16:46:00 +0100

    Add repositories section

---
+++ src/scripts/pages/version-control/repositories.js
@@ -0,0 +1,5 @@
+import { setNavigationButtons, setTitle } from "/scripts/modules/common_templates.js";
+
+setTitle("Git Repositories", "A way to organize and version snapshots of your code")
+setNavigationButtons("/", "/version-control/commits.html")
+
