b8fcaed9f664313d4b63016575b1cf83f318478d
Paulo Sánchez <dev.szblzpaulo@gmail.com>
2023-12-17 23:15:15 +0100

    Reformat code and add new lessons' chapter

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,7 +5,9 @@ setTitle("Aerotech Programming Course", "The course where you'll learn the basic
 
 Handlebars.partials = Handlebars.templates;
 
-let envContentCards = [
+// Environment Setup Content Cards:
+
+let environmentSetupCCs = [
     {
         img_source: "./public/icons8/git.png",
         title: "Git setup",
@@ -35,101 +37,17 @@ let envContentCards = [
         link: "/environment-setup/github-desktop.html"
     }
 ];
-envContentCards.forEach((card, index) => card.title = `${index + 1}. ${card.title}`);
+environmentSetupCCs.forEach((card, index) => card.title = `${index + 1}. ${card.title}`);
 
-const envContentCardsDiv = {
+const environmentSetupCCsDiv = {
     title: "Progression",
     description: "The following steps will guide you through the process of setting up your environment. Try to follow them in order to avoid any configuration issues.",
-    content_cards: envContentCards
+    content_cards: environmentSetupCCs
 };
 
-let template = Handlebars.templates.content_card_collection;
-document.getElementById("env_content_cards").innerHTML = template(envContentCardsDiv);
+// Version Control Content Cards:
 
-const pyContentCards = [
-    {
-        img_source: "./public/icons8/data-types.png",
-        title: "Data types",
-        description: "Learn about the different data types in Python",
-        issue_link: generateURL("python", "data"),
-        link: "/python-programming/data-types.html"
-    },
-    {
-        img_source: "./public/icons8/operators.png",
-        title: "Operators",
-        description: "Understand different operators and operation preference",
-        issue_link: generateURL("python", "operators"),
-        link: "/python-programming/operators.html"
-    },
-    {
-        img_source: "./public/icons8/conditional-structures.png",
-        title: "Conditional structures",
-        description: "Study conditional structures syntax and usage",
-        issue_link: generateURL("python", "conditional"),
-        link: "/python-programming/conditional-structures.html"
-    },
-    {
-        img_source: "./public/icons8/iteration-structures.png",
-        title: "Iteration structures",
-        description: "Study iteration structures syntax and usage",
-        issue_link: generateURL("python", "iteration"),
-        link: "/python-programming/iteration-structures.html"
-    },
-    {
-        img_source: "./public/icons8/functions.png",
-        title: "Functions",
-        description: "Understand how functions work and how to create custom ones",
-        issue_link: generateURL("python", "functions"),
-        link: "/python-programming/functions.html"
-    },
-    {
-        img_source: "./public/icons8/classes.png",
-        title: "Classes",
-        description: "Learn about classes and the OOP paradigm in Python",
-        issue_link: generateURL("python", "classes"),
-        link: "/python-programming/classes.html"
-    },
-    {
-        img_source: "./public/icons8/exceptions.png",
-        title: "Exceptions",
-        description: "Understand how exceptions work and how to handle them",
-        issue_link: generateURL("python", "exceptions"),
-        link: "/python-programming/exceptions.html"
-    },
-    {
-        img_source: "./public/icons8/libraries.png",
-        title: "Libraries",
-        description: "Discover some of the most useful libraries in Python and learn to use them",
-        issue_link: generateURL("python", "libraries"),
-        link: "/python-programming/libraries.html"
-    },
-    {
-        img_source: "./public/icons8/code-style.png",
-        title: "Code style",
-        description: "Learn how to style your code to make it more readable and maintainable",
-        issue_link: generateURL("python", "code"),
-        link: "/python-programming/code-style.html"
-    },
-    {
-        img_source: "./public/icons8/project.png",
-        title: "Project",
-        description: "Apply your knowledge to a real-world project",
-        issue_link: generateURL("python", "project"),
-        link: "/python-programming/project.html"
-    }
-];
-pyContentCards.forEach((card, index) => card.title = `${index + 1}. ${card.title}`);
-
-const pyContentCardsDiv = {
-    title: "Progression",
-    description: "The following steps will guide you through the process of learning Python up to an intermediate level. Try to follow them in order to avoid any confusion.",
-    content_cards: pyContentCards
-};
-
-template = Handlebars.templates.content_card_collection;
-document.getElementById("py_content_cards").innerHTML = template(pyContentCardsDiv);
-
-const verContentCards = [
+const versionControlCCs = [
     {
         img_source: "./public/icons8/repositories.png",
         title: "Git Repositories",
@@ -201,16 +119,111 @@ const verContentCards = [
         link: "/version-control/projects.html"
     }
 ];
-verContentCards.forEach((card, index) => card.title = `${index + 1}. ${card.title}`);
+versionControlCCs.forEach((card, index) => card.title = `${index + 1}. ${card.title}`);
 
-const verContentCardsDiv = {
+const versionControlCCsDiv = {
     title: "Progression",
     description: "The following steps will guide you through the process of understanding how version control works. It is recommended to follow them in order to understand the concepts better.",
-    content_cards: verContentCards
+    content_cards: versionControlCCs
 };
 
-template = Handlebars.templates.content_card_collection;
-document.getElementById("ver_content_cards").innerHTML = template(verContentCardsDiv);
+// Python Programming Content Cards:
+
+const pythonProgrammingCCs = [
+    {
+        img_source: "./public/icons8/introduction.png",
+        title: "Introduction",
+        description: "General information about Python and its usage",
+        issue_link: generateURL("python", "data"),  // FIXME: This is a placeholder
+        link: "/python-programming/introduction.html"
+    },
+    {
+        img_source: "./public/icons8/data-types.png",
+        title: "Data types",
+        description: "Learn about the different data types in Python",
+        issue_link: generateURL("python", "data"),
+        link: "/python-programming/data-types.html"
+    },
+    {
+        img_source: "./public/icons8/operators.png",
+        title: "Operators",
+        description: "Understand different operators and operation preference",
+        issue_link: generateURL("python", "operators"),
+        link: "/python-programming/operators.html"
+    },
+    {
+        img_source: "./public/icons8/conditional-structures.png",
+        title: "Conditional structures",
+        description: "Study conditional structures syntax and usage",
+        issue_link: generateURL("python", "conditional"),
+        link: "/python-programming/conditional-structures.html"
+    },
+    {
+        img_source: "./public/icons8/iteration-structures.png",
+        title: "Iteration structures",
+        description: "Study iteration structures syntax and usage",
+        issue_link: generateURL("python", "iteration"),
+        link: "/python-programming/iteration-structures.html"
+    },
+    {
+        img_source: "./public/icons8/functions.png",
+        title: "Functions",
+        description: "Understand how functions work and how to create custom ones",
+        issue_link: generateURL("python", "functions"),
+        link: "/python-programming/functions.html"
+    },
+    {
+        img_source: "./public/icons8/classes.png",
+        title: "Classes",
+        description: "Learn about classes and the OOP paradigm in Python",
+        issue_link: generateURL("python", "classes"),
+        link: "/python-programming/classes.html"
+    },
+    {
+        img_source: "./public/icons8/exceptions.png",
+        title: "Exceptions",
+        description: "Understand how exceptions work and how to handle them",
+        issue_link: generateURL("python", "exceptions"),
+        link: "/python-programming/exceptions.html"
+    },
+    {
+        img_source: "./public/icons8/libraries.png",
+        title: "Libraries",
+        description: "Discover some of the most useful libraries in Python and learn to use them",
+        issue_link: generateURL("python", "libraries"),
+        link: "/python-programming/libraries.html"
+    },
+    {
+        img_source: "./public/icons8/code-style.png",
+        title: "Code style",
+        description: "Learn how to style your code to make it more readable and maintainable",
+        issue_link: generateURL("python", "code"),
+        link: "/python-programming/code-style.html"
+    },
+    {
+        img_source: "./public/icons8/project.png",
+        title: "Project",
+        description: "Apply your knowledge to a real-world project",
+        issue_link: generateURL("python", "project"),
+        link: "/python-programming/project.html"
+    }
+];
+
+pythonProgrammingCCs.forEach((card, index) => card.title = `${index + 1}. ${card.title}`);
+
+const pythonProgrammingCCsDiv = {
+    title: "Progression",
+    description: "The following steps will guide you through the process of learning Python up to an intermediate level. Try to follow them in order to avoid any confusion.",
+    content_cards: pythonProgrammingCCs
+};
+
+// Template resolution:
+
+document.getElementById("env_content_cards").innerHTML = Handlebars.templates.content_card_collection(environmentSetupCCsDiv);
+document.getElementById("ver_content_cards").innerHTML = Handlebars.templates.content_card_collection(versionControlCCsDiv);
+document.getElementById("py_content_cards").innerHTML = Handlebars.templates.content_card_collection(pythonProgrammingCCsDiv);
+
+// Localhost locked elements:
 
 const lockedElements2 = Array.from(document.getElementsByClassName("locked"));
 
