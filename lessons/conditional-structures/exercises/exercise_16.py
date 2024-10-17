f10b30d70358159b7c32602c16ec2373088025b8
ElBrokas68 <salgadobreixo@gmail.com>
2024-10-17 17:44:40 +0200

    Exercise 16

--- lessons/conditional-structures/exercises/exercise_16.py
+++ lessons/conditional-structures/exercises/exercise_16.py
@@ -1,1 +1,12 @@
 n = 2  # Do not modify this line.
+
+if n > 0:
+    message = "The number is positive"
+elif n < 0:
+    message = "The number is negative"
+else:
+    message = "The number is zero"
+if n % 2 == 0:
+    message += " and even"
+else:
+    message += " and odd"
