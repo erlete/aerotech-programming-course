b4871417bbb679843df6142495db5a7ea07b3155
ElBrokas68 <salgadobreixo@gmail.com>
2024-10-17 18:03:11 +0200

    Exercise 17

--- lessons/iteration-structures/exercises/exercise_17.py
+++ lessons/iteration-structures/exercises/exercise_17.py
@@ -1,1 +1,13 @@
 data = list(range(1, 101))  # Do not modify this line.
+
+log = []
+for i in data:
+    if i % 3 == 0 and i % 5 == 0:
+        log.append("FizzBuzz")
+    elif i % 3 == 0:
+        log.append("Fizz")
+    elif i % 5 == 0:
+        log.append("Buzz")
+    else:
+        log.append("idk")
+print(log)
